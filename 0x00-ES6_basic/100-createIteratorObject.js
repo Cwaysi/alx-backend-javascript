f744f43d0263951b7559055382604dd7f05e0791
Justice Morison Gurah <cwaysimorison@outlook.com>
2023-07-24 17:07:05 +0000

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -10,16 +10,14 @@ export default function createIteratorObject(report) {
   const iterator = {
     next() {
       if (index < allEmployees.length) {
-        // If there are employees left, return the next one
+
         return { value: allEmployees[index++], done: false };
       } else {
-        // If all employees have been traversed, return done:true
         return { done: true };
       }
     },
   };
 
-  // Make the iterator iterable by adding Symbol.iterator method
   iterator[Symbol.iterator] = function () {
     return this;
   };
