2f9fab5f9b99602e0403ffaeff7c049b4926cf15
Justice Morison Gurah <cwaysimorison@outlook.com>
2023-08-01 20:56:06 +0000

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,21 @@
-const getListStudents () => [
-  {
+export default function getListStudents() {
+  const fi = {
     id: 1,
     firstName: 'Guillaume',
     location: 'San Francisco',
-  },
-  {
+  };
+
+  const se = {
     id: 2,
     firstName: 'James',
     location: 'Columbia',
-  },
-  {
+  };
+
+  const th = {
     id: 5,
     firstName: 'Serena',
     location: 'San Francisco',
-  },
-];
+  };
 
-export default getListStudents;
+  return [fi, se, th];
+}
